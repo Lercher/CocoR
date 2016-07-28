3a64581e0ac8b8197f3bee32e52928f4556991be
Lercher <lercher@singhammer.de>
2016-07-28 17:17:34 +0200

    tokens and relevant source will be displayed

--- CSharp/test/main.cs
+++ CSharp/test/main.cs
@@ -11,10 +11,25 @@ public class Inheritance {
             Scanner scanner = new Scanner(arg[0]);
 			Parser parser = new Parser(scanner);
             parser.Parse();
-            Console.WriteLine("{0} error(s) detected", parser.errors.count);            
+            Console.WriteLine("{0} error(s) detected", parser.errors.count);
+            int line = 0;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
             foreach (Token t in parser.tokens)
             {
-                Console.WriteLine("({0,3},{1,3}) {2,3} {3,-20} {4}", t.line, t.col, t.kind, Parser.tName[t.kind], t.val);
+                if (line == 0) {                    
+                    sb.Append(new string('-', t.col + 50));
+                    sb.Append("  ");
+                    line = t.line;
+                }
+                if (line != t.line) {
+                    line = t.line;
+                    Console.WriteLine(sb.ToString());
+                    sb.Length = 0;
+                    sb.Append(new string('-', t.col + 50));
+                    sb.Append("  ");
+                }
+                sb.Append(t.val); sb.Append(' ');
+                Console.WriteLine("({0,3},{1,3}) {2,3} {3,-20} {4}", t.line, t.col, t.kind, Parser.tName[t.kind], t.val);                
             }
             if (parser.errors.count > 0)
                 return 1;
