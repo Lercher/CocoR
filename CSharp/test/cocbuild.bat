22c65c523ce0ad9a4f8d0d9e2a1c7e82cbb06207
Martin Lercher <Lercher@singhammer.de>
2016-07-31 00:47:02 +0200

    don't execute if error

--- CSharp/test/cocbuild.bat
+++ CSharp/test/cocbuild.bat
@@ -1,5 +1,7 @@
 ..\coco inheritance.ATG -frames .. -ac
 if not errorlevel 1 (
     %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:inheritance.exe /t:exe *.cs
-    inheritance.exe sample.txt
+    if not errorlevel 1 (
+        inheritance.exe sample.txt
+    )
 )
