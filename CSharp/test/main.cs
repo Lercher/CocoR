54c5120849e11c60ae9677d778f8d23d054308cf
Lercher <lercher@singhammer.de>
2016-08-02 21:45:09 +0200

    autocomplete information print optional

--- CSharp/test/main.cs
+++ CSharp/test/main.cs
@@ -18,7 +18,7 @@ public class Inheritance {
 
 	public static int Main (string[] arg) {
 		Console.WriteLine("Inheritance parser");
-        if (arg.Length == 1)
+        if (arg.Length >= 1)
         {
             Console.WriteLine("scanning {0} ...", arg[0]);
             Scanner scanner = new Scanner(arg[0], true); // is UTF8 source
@@ -30,49 +30,51 @@ public class Inheritance {
             printST(parser.types);
             printST(parser.variables);
 
-            // list all alternatives
-            int line = 0;
-            System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            foreach (Alternative a in parser.tokens)
-            {
-                Token t = a.t;
-                if (line == 0) {                    
-                    sb.Append(new string('-', 50));
-                    sb.Append(new string(' ', t.col));
-                    sb.Append("  ");
-                    line = t.line;
-                }
-                if (line != t.line) {
-                    line = t.line;
-                    Console.WriteLine(sb.ToString());
-                    sb.Length = 0;
-                    sb.Append(new string('-', 50));
-                    sb.Append(new string(' ', t.col));
-                    sb.Append("  ");
-                }
-                sb.Append(t.val); sb.Append(' ');
-                string decl = a.declaration == null ? "" : string.Format(" declared({0},{1})", a.declaration.line, a.declaration.col);
-                Console.Write("({0,3},{1,3}) {2,3} {3,-30} {4, -20}", t.line, t.col, t.kind, Parser.tName[t.kind] + decl, t.val);
-                Console.Write("      alt: ");
-                for (int k = 0; k <= Parser.maxT; k++)
+            if (arg.Length > 1) {
+                // list all alternatives
+                int line = 0;
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                foreach (Alternative a in parser.tokens)
                 {
-                    if (a.alt[k]) {
-                        Console.Write("{1}", k, Parser.tName[k]);
-                        if (a.st[k] != null) {
-                            Console.Write(":{0}({1})|", a.st[k].name, a.st[k].CountScopes);
-                            foreach (Token tok in a.st[k].currentScope)
-                                Console.Write("{0}({1},{2})|", tok.val, tok.line, tok.col);    
-                        }
-                        Console.Write(' ');
+                    Token t = a.t;
+                    if (line == 0) {                    
+                        sb.Append(new string('-', 50));
+                        sb.Append(new string(' ', t.col));
+                        sb.Append("  ");
+                        line = t.line;
                     }
+                    if (line != t.line) {
+                        line = t.line;
+                        Console.WriteLine(sb.ToString());
+                        sb.Length = 0;
+                        sb.Append(new string('-', 50));
+                        sb.Append(new string(' ', t.col));
+                        sb.Append("  ");
+                    }
+                    sb.Append(t.val); sb.Append(' ');
+                    string decl = a.declaration == null ? "" : string.Format(" declared({0},{1})", a.declaration.line, a.declaration.col);
+                    Console.Write("({0,3},{1,3}) {2,3} {3,-30} {4, -20}", t.line, t.col, t.kind, Parser.tName[t.kind] + decl, t.val);
+                    Console.Write("      alt: ");
+                    for (int k = 0; k <= Parser.maxT; k++)
+                    {
+                        if (a.alt[k]) {
+                            Console.Write("{1}", k, Parser.tName[k]);
+                            if (a.st[k] != null) {
+                                Console.Write(":{0}({1})|", a.st[k].name, a.st[k].CountScopes);
+                                foreach (Token tok in a.st[k].currentScope)
+                                    Console.Write("{0}({1},{2})|", tok.val, tok.line, tok.col);    
+                            }
+                            Console.Write(' ');
+                        }
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+                Console.WriteLine(sb.ToString());
             }
-            Console.WriteLine(sb.ToString());
             if (parser.errors.count > 0)
                 return 1;
         } else {
-            Console.WriteLine("usage: Inheritance.exe file");
+            Console.WriteLine("usage: Inheritance.exe file [-ac]");
             return 99;
         }
         return 0;
