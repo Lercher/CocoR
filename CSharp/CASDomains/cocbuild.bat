d27bf3667003d38dc737fd80ca7fd22b27991e3a
Martin Lercher <Lercher@singhammer.de>
2016-08-03 02:04:56 +0200

    editor for casdomains

--- CSharp/CASDomains/cocbuild.bat
+++ CSharp/CASDomains/cocbuild.bat
@@ -1,7 +1,11 @@
 ..\coco CasDomains.atg -frames .. -ac -is -utf8
 if not errorlevel 1 (
-    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe *.cs
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
         CasDomains.exe sample.txt
+    )
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
+    if not errorlevel 1 (
+        CasDomainsEditor.exe
     )
 )
