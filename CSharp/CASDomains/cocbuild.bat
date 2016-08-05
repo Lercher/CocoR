1aeebefb7644c2665c5854ca5323f05da25a70f6
Martin Lercher <Lercher@singhammer.de>
2016-08-06 01:58:17 +0200

    single build

--- CSharp/CASDomains/cocbuild.bat
+++ CSharp/CASDomains/cocbuild.bat
@@ -1,9 +1,9 @@
 ..\coco CasDomains.atg -frames .. -ac -is -utf8
 if not errorlevel 1 (
-    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe Main.cs Lib.cs Parser.cs Scanner.cs
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
         CasDomains.exe sample.txt
-        %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomainsEditor.exe /t:exe Editor.cs Lib.cs Parser.cs Scanner.cs
+        %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
         if not errorlevel 1 (
             rem copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
             rem copy /y *.txt \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
---
+++ CSharp/CASDomains/cocbuild1.bat
@@ -0,0 +1,7 @@
+..\coco CasDomains.atg -frames .. -ac -is -utf8
+if not errorlevel 1 (
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
+    if not errorlevel 1 (
+        CasDomains.exe sample.txt
+    )
+)
