c27102a062abffedfc87f08fb3fe7ed342f1ffea
Martin Lercher <Lercher@singhammer.de>
2016-08-06 13:25:51 +0200

    debug build

--- CSharp/CASDomains/cocbuild.bat
+++ CSharp/CASDomains/cocbuild.bat
@@ -1,7 +1,7 @@
 ..\coco CasDomains.atg -frames .. -ac -utf8
 if not errorlevel 1 (
     CasDomains.exe sample.txt
-    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
         rem copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
         rem copy /y *.txt \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
--- CSharp/CASDomains/cocbuild1.bat
+++ CSharp/CASDomains/cocbuild1.bat
@@ -1,6 +1,6 @@
 ..\coco CasDomains.atg -frames .. -ac -utf8
 if not errorlevel 1 (
-    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
+    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
         CasDomains.exe sample.txt
     )
