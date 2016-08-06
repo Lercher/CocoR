53cc6ff45447a2064d861e35810a4f6b9794eb70
Martin Lercher <Lercher@singhammer.de>
2016-08-06 13:56:58 +0200

    build

--- CSharp/CASDomains/cocbuild.bat
+++ CSharp/CASDomains/cocbuild.bat
@@ -1,6 +1,5 @@
 ..\coco CasDomains.atg -frames .. -ac -utf8
 if not errorlevel 1 (
-    CasDomains.exe sample.txt
     %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
         rem copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
