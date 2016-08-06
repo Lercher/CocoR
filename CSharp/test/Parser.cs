dd77ca597a6319d9bcb121bc6dd43f32fe466613
Martin Lercher <Lercher@singhammer.de>
2016-08-06 19:58:22 +0200

    diagnostic output removed

--- CSharp/test/Parser.cs
+++ CSharp/test/Parser.cs
@@ -1173,10 +1173,7 @@ public abstract class AST {
 
         public E add(E e) {
             if (name == e.name) {
-				if (name == null) 
-					Console.WriteLine(" [merge two unnamed to a single list]");
-				else
-					Console.WriteLine(" [merge two named {0} to a single list]", name);
+				//if (name == null) Console.WriteLine(" [merge two unnamed to a single list]"); else Console.WriteLine(" [merge two named {0} to a single list]", name);
                 ASTList list = new ASTList(ast);
                 list.merge(e.ast);
                 E ret = new E();
@@ -1184,7 +1181,7 @@ public abstract class AST {
                 ret.name = name;
                 return ret;
             } else if (name != null && e.name != null) {
-				Console.WriteLine(" [merge named {0}+{1} to an unnamed object]", name, e.name);
+				//Console.WriteLine(" [merge named {0}+{1} to an unnamed object]", name, e.name);
                 ASTObject obj = new ASTObject();
                 obj.add(this);
                 obj.add(e);
@@ -1192,10 +1189,10 @@ public abstract class AST {
                 ret.ast = obj;
                 return ret;
             } else if (ast.merge(e)) {
-				Console.WriteLine(" [merged {1} into object {0}]", name, e.name);
+				//Console.WriteLine(" [merged {1} into object {0}]", name, e.name);
                 return this;
 			}
-			Console.WriteLine(" [no merge available for {0}+{1}]", name, e.name);
+			//Console.WriteLine(" [no merge available for {0}+{1}]", name, e.name);
             return null;
         }
 
@@ -1207,7 +1204,6 @@ public abstract class AST {
     public class Builder {
         public readonly Parser parser;
         private readonly Stack<E> stack = new Stack<E>();
-		public readonly List<AST> mergeconflicts = new List<AST>(); 
 
         public Builder(Parser parser) {
             this.parser = parser;
@@ -1230,12 +1226,12 @@ public abstract class AST {
 
         private void push(E e) {
             stack.Push(e);
-            System.Console.WriteLine("-> push {0}, size {1}", e, stack.Count);
+            //System.Console.WriteLine("-> push {0}, size {1}", e, stack.Count);
         }
 
         // that's what we call for #/##, built from an AstOp
         public void hatch(Token t, string literal, string name, bool islist) {
-            System.Console.WriteLine(">> hatch token {0,-20} as {2,-10}, islist {3}, literal:{1} at {4},{5}.", t.val, literal, name, islist, t.line, t.col);
+            //System.Console.WriteLine(">> hatch token {0,-20} as {2,-10}, islist {3}, literal:{1} at {4},{5}.", t.val, literal, name, islist, t.line, t.col);
             E e = new E();
             e.ast = new ASTLiteral(literal != null ? literal : t.val);
             if (islist)
@@ -1244,14 +1240,11 @@ public abstract class AST {
             push(e);
         }
 
-        // that's what we call for ^, built from an AstOp
+        // that's what we call for ^/^^, built from an AstOp
         public void sendup(Token t, string literal, string name, bool islist) {
 			if (stack.Count == 0) return;
             E e = currentE;
-            if (islist)
-                System.Console.WriteLine(">> send up as [{0}]: {1}", name, e);
-            else
-                System.Console.WriteLine(">> send up as {0}: {1}", name, e);
+            //if (islist) System.Console.WriteLine(">> send up as [{0}]: {1}", name, e); else System.Console.WriteLine(">> send up as {0}: {1}", name, e);
             if (name != e.name) {
                 if (islist)
                     e.wrapinlist(); 
@@ -1259,13 +1252,14 @@ public abstract class AST {
                     parser.errors.Warning(t.line, t.col, string.Format("overwriting AST objectname '{0}' with '{1}'", e.name, name));
             }
             e.name = name;
-            System.Console.WriteLine("-------------> top {0}", e);
+            //System.Console.WriteLine("-------------> top {0}", e);
         }
 
+		/*
 		private void mergeConflict(Token t, E e, E with, string typ, int n) {
-			mergeconflicts.Add(e.ast);
 			parser.errors.Warning(t.line, t.col, string.Format("AST merge {2} size {3}: {0} WITH {1}", e, with, typ, n));
-		}
+		} 
+		*/
 
 		public void popNull() {
 			if (stack.Count == 0) return; // should not happen.
@@ -1303,22 +1297,22 @@ public abstract class AST {
 			int n = 0;
 			foreach(E e in list) {
 				n++;
-				System.Console.Write("{3}>> {1} of {2}   merge: {0}", e, n, cnt, stack.Count);
+				//System.Console.Write("{3}>> {1} of {2}   merge: {0}", e, n, cnt, stack.Count);
 				if (ret == null) 
 					ret = e;
 				else {
 					E merged = ret.add(e);
 					if (merged != null) {
 						somethingMerged = true;
-						mergeConflict(t, e, ret, "success", stack.Count);
+						//mergeConflict(t, e, ret, "success", stack.Count);
 						ret = merged;
 					} else {
-						mergeConflict(t, e, ret, "conflict", stack.Count);
+						//mergeConflict(t, e, ret, "conflict", stack.Count);
 						push(ret);
 						ret = e; 
 					}                    
 				}
-				System.Console.WriteLine(" -> ret={0}", ret);
+				//System.Console.WriteLine(" -> ret={0}", ret);
 			}
 			push(ret);
 			return somethingMerged;
