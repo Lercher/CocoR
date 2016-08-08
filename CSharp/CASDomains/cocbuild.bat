0156324bd0239e15e9b8a282eac35a276baf5ff4
Lercher <lercher@singhammer.de>
2016-08-08 14:01:17 +0200

    deployment

--- CSharp/CASDomains/cocbuild.bat
+++ CSharp/CASDomains/cocbuild.bat
@@ -2,9 +2,9 @@
 if not errorlevel 1 (
     %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasDomainsEditor.exe /t:exe Editor.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
-        rem copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
-        rem copy /y *.txt \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
-        rem copy /y *.atg \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
+        copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
+        copy /y *.txt \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
+        copy /y *.atg \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
         CasDomainsEditor.exe            
     )
 )
--- CSharp/CASDomains/cocbuild1.bat
+++ CSharp/CASDomains/cocbuild1.bat
@@ -2,6 +2,9 @@
 if not errorlevel 1 (
     %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasDomains.exe /t:exe Main.cs Parser.cs Scanner.cs
     if not errorlevel 1 (
+        copy /y *.exe \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
+        copy /y *.txt \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
+        copy /y *.atg \\ntsdtsc.singhammer.de\Leasing\bin\CAS\
         CasDomains.exe sample.txt
     )
 )
