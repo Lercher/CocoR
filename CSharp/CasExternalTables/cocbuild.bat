9dfe05de1a1588c60e23a8df0e8924f93bc8a682
Martin Lercher <martin.lercher@gmx.de>
2017-04-28 18:44:10 +0200

    Error Classifications

--- CSharp/CasExternalTables/cocbuild.bat
+++ CSharp/CasExternalTables/cocbuild.bat
@@ -1,6 +1,6 @@
 ..\coco CasExternalTables.atg -frames .. -ac -utf8
 if not errorlevel 1 (
-    %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasExternalTables.exe /t:exe *.cs
+    %windir%\Microsoft.Net\Framework\v4.0.30319\csc.exe /debug /out:CasExternalTables.exe /t:exe *.cs
     if not errorlevel 1 if /%1/==// (
         CasExternalTables.exe "\\ntsdtsc\leasing\TEMP\Lercher\UPDATE_EXTERNALTABLES.SQL"
     )
