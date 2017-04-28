28bb57382be8667cc01e9f17b326d235134e085f
Martin Lercher <martin.lercher@gmx.de>
2017-04-28 16:20:00 +0200

    Back to internal tables

--- CSharp/CasExternalTables/cocbuild.bat
+++ CSharp/CasExternalTables/cocbuild.bat
@@ -2,6 +2,6 @@
 if not errorlevel 1 (
     %windir%\Microsoft.Net\Framework\v2.0.50727\csc.exe /debug /out:CasExternalTables.exe /t:exe *.cs
     if not errorlevel 1 if /%1/==// (
-        CasExternalTables.exe "\\ntsdtsc\leasing\TEMP\Lercher\UPDATE_INTERNALTABLES.SQL"
+        CasExternalTables.exe "\\ntsdtsc\leasing\TEMP\Lercher\UPDATE_EXTERNALTABLES.SQL"
     )
 )
