01f4437176a9b66f6798a63bc6442471f440109a
Martin Lercher <Lercher@singhammer.de>
2017-05-14 20:04:34 +0200

    pretty print and current date

--- CocoRCore/src/Coco.cs
+++ CocoRCore/src/Coco.cs
@@ -43,97 +43,109 @@ Coco/R itself) does not fall under the GNU General Public License.
 using System;
 using System.IO;
 
-namespace at.jku.ssw.Coco {
+namespace at.jku.ssw.Coco
+{
 
-public class Coco {
-		
-	public static int Main (string[] arg) {
-		Console.Write("Coco/R (Jul 28, 2016)");
-		string srcName = null, nsName = null, frameDir = null, ddtString = null,
-		traceFileName = null, outDir = null;
-		bool emitLines = false, generateAutocompleteInformation = false, ignoreSemanticActions = false, 
-		isUTF8 = false;
-		int retVal = 1;
-		for (int i = 0; i < arg.Length; i++) {
-			if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i].Trim();
-			else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i].Trim();
-			else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i].Trim();
-			else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i].Trim();
-			else if (arg[i] == "-lines") emitLines = true;
-			else if (arg[i] == "-ac") generateAutocompleteInformation = true;
-			else if (arg[i] == "-is") ignoreSemanticActions = true;
-			else if (arg[i] == "-utf8") isUTF8 = true;
-			else srcName = arg[i];
-		}
-		if (emitLines) Console.Write(" [emit lines]");
-		if (generateAutocompleteInformation) Console.Write(" [generate autocomplete information]");
-		if (ignoreSemanticActions) Console.Write(" [ignore semantic actions]");
-		if (isUTF8) Console.Write(" [forced UTF8 processing]");
-		Console.WriteLine();
-		if (arg.Length > 0 && srcName != null) {
-			try {
-				string srcDir = Path.GetDirectoryName(srcName);
-				
-				Scanner scanner = new Scanner(srcName, isUTF8);
-				Parser parser = new Parser(scanner);
+    public class Coco
+    {
 
-				traceFileName = Path.Combine(srcDir, "trace.txt");
-				parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
-				parser.tab = new Tab(parser);
-				parser.dfa = new DFA(parser);
-				parser.pgen = new ParserGen(parser);
-				parser.pgen.GenerateAutocompleteInformation = generateAutocompleteInformation;
-				parser.pgen.IgnoreSemanticActions = ignoreSemanticActions;
+        public static int Main(string[] arg)
+        {
+            Console.Write("Coco/R Core (May 14, 2017)");
+            string srcName = null, nsName = null, frameDir = null, ddtString = null,
+            traceFileName = null, outDir = null;
+            bool emitLines = false, generateAutocompleteInformation = false, ignoreSemanticActions = false,
+            isUTF8 = false;
+            int retVal = 1;
+            for (int i = 0; i < arg.Length; i++)
+            {
+                if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i].Trim();
+                else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i].Trim();
+                else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i].Trim();
+                else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i].Trim();
+                else if (arg[i] == "-lines") emitLines = true;
+                else if (arg[i] == "-ac") generateAutocompleteInformation = true;
+                else if (arg[i] == "-is") ignoreSemanticActions = true;
+                else if (arg[i] == "-utf8") isUTF8 = true;
+                else srcName = arg[i];
+            }
+            if (emitLines) Console.Write(" [emit lines]");
+            if (generateAutocompleteInformation) Console.Write(" [generate autocomplete information]");
+            if (ignoreSemanticActions) Console.Write(" [ignore semantic actions]");
+            if (isUTF8) Console.Write(" [forced UTF8 processing]");
+            Console.WriteLine();
+            if (arg.Length > 0 && srcName != null)
+            {
+                try
+                {
+                    string srcDir = Path.GetDirectoryName(srcName);
 
-				parser.tab.srcName = srcName;
-				parser.tab.srcDir = srcDir;
-				parser.tab.nsName = nsName;
-				parser.tab.frameDir = frameDir;
-				parser.tab.outDir = (outDir != null) ? outDir : srcDir;
-				parser.tab.emitLines = emitLines;
-				if (ddtString != null) parser.tab.SetDDT(ddtString);
+                    Scanner scanner = new Scanner(srcName, isUTF8);
+                    Parser parser = new Parser(scanner);
 
-				parser.Parse();
+                    traceFileName = Path.Combine(srcDir, "trace.txt");
+                    parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
+                    parser.tab = new Tab(parser);
+                    parser.dfa = new DFA(parser);
+                    parser.pgen = new ParserGen(parser);
+                    parser.pgen.GenerateAutocompleteInformation = generateAutocompleteInformation;
+                    parser.pgen.IgnoreSemanticActions = ignoreSemanticActions;
 
-				Console.WriteLine("grammar scanned by using {0}", scanner.buffer.GetType().Name);
-				parser.trace.Dispose();
-				FileInfo f = new FileInfo(traceFileName);
-				if (f.Length == 0) f.Delete();
-				else Console.WriteLine("trace output is in " + traceFileName);
-				Console.WriteLine("{0} error(s) detected", parser.errors.count);
-				if (parser.errors.count == 0) { retVal = 0; }
-			} catch (IOException) {
-				Console.WriteLine("-- could not open " + traceFileName);
-			} catch (FatalError e) {
-				Console.WriteLine("-- " + e.Message);
-			}
-		} else {
-			Console.WriteLine("Usage: Coco Grammar.ATG {{Option}}{0}"
-			                  + "Options:{0}"
-			                  + "  -namespace <namespaceName>{0}"
-			                  + "  -frames    <frameFilesDirectory>{0}"
-			                  + "  -trace     <traceString>{0}"
-			                  + "  -o         <outputDirectory>{0}"
-			                  + "  -lines     [emit lines]{0}"
-							  + "  -ac        [generate autocomplete/intellisense information]{0}"
-							  + "  -is        [ignore semantic actions]{0}"
-							  + "  -utf8      [force UTF-8 processing, even without BOM]{0}"
-			                  + "Valid characters in the trace string:{0}"
-			                  + "  A  trace automaton{0}"
-			                  + "  F  list first/follow sets{0}"
-			                  + "  G  print syntax graph{0}"
-			                  + "  I  trace computation of first sets{0}"
-			                  + "  J  list ANY and SYNC sets{0}"
-			                  + "  P  print statistics{0}"
-			                  + "  S  list symbol table{0}"
-			                  + "  X  list cross reference table{0}"
-			                  + "Scanner.frame and Parser.frame files needed in ATG directory{0}"
-			                  + "or in a directory specified in the -frames option.",
-			                  Environment.NewLine);
-		}
-		return retVal;
-	}
-		
-} // end Coco
+                    parser.tab.srcName = srcName;
+                    parser.tab.srcDir = srcDir;
+                    parser.tab.nsName = nsName;
+                    parser.tab.frameDir = frameDir;
+                    parser.tab.outDir = (outDir != null) ? outDir : srcDir;
+                    parser.tab.emitLines = emitLines;
+                    if (ddtString != null) parser.tab.SetDDT(ddtString);
+
+                    parser.Parse();
+
+                    Console.WriteLine("grammar scanned by using {0}", scanner.buffer.GetType().Name);
+                    parser.trace.Dispose();
+                    FileInfo f = new FileInfo(traceFileName);
+                    if (f.Length == 0) f.Delete();
+                    else Console.WriteLine("trace output is in " + traceFileName);
+                    Console.WriteLine("{0} error(s) detected", parser.errors.count);
+                    if (parser.errors.count == 0) { retVal = 0; }
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("-- could not open " + traceFileName);
+                }
+                catch (FatalError e)
+                {
+                    Console.WriteLine("-- " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: Coco Grammar.ATG {{Option}}{0}"
+                                  + "Options:{0}"
+                                  + "  -namespace <namespaceName>{0}"
+                                  + "  -frames    <frameFilesDirectory>{0}"
+                                  + "  -trace     <traceString>{0}"
+                                  + "  -o         <outputDirectory>{0}"
+                                  + "  -lines     [emit lines]{0}"
+                                  + "  -ac        [generate autocomplete/intellisense information]{0}"
+                                  + "  -is        [ignore semantic actions]{0}"
+                                  + "  -utf8      [force UTF-8 processing, even without BOM]{0}"
+                                  + "Valid characters in the trace string:{0}"
+                                  + "  A  trace automaton{0}"
+                                  + "  F  list first/follow sets{0}"
+                                  + "  G  print syntax graph{0}"
+                                  + "  I  trace computation of first sets{0}"
+                                  + "  J  list ANY and SYNC sets{0}"
+                                  + "  P  print statistics{0}"
+                                  + "  S  list symbol table{0}"
+                                  + "  X  list cross reference table{0}"
+                                  + "Scanner.frame and Parser.frame files needed in ATG directory{0}"
+                                  + "or in a directory specified in the -frames option.",
+                                  Environment.NewLine);
+            }
+            return retVal;
+        }
+
+    } // end Coco
 
 } // end namespace
