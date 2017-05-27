716b70fc505f3eaa06ac9c436c1ed36d4c3bc7b6
Martin Lercher <Lercher@singhammer.de>
2017-05-27 02:46:25 +0200

    BugFix and more umlauts

--- CocoRCore/sample-grammars/Taste/Taste.cs
+++ CocoRCore/sample-grammars/Taste/Taste.cs
@@ -7,8 +7,8 @@ namespace CocoRCore.Samples.Taste
 
         public static ParserBase Create(string arg)
         {
-            Scanner scanner = new Scanner().Initialize(arg);
-            Parser parser = new Parser(scanner);
+            var scanner = new Scanner().Initialize(arg);
+            var parser = new Parser(scanner);
             parser.tab = new SymbolTable(parser);
             parser.gen = new CodeGenerator();
             return parser;
